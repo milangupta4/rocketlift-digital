f5a8b3b8365af0163776fb8b133e3d4e3639a7b0
Milan Gupta <milan.gupta.iitd@gmail.com>
2025-02-12 09:43:30 +0530

    Sitemap fixes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://rocketlift.co',
+  siteUrl: process.env.SITE_URL || 'https://www.rocketlift.co',
   generateRobotsTxt: true,
   generateIndexSitemap: true,
   exclude: ['/404'],

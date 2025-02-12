6f1fe892c73aadd9e26aaa5e55cdcc76692b877b
Milan Gupta <milan.gupta.iitd@gmail.com>
2025-02-12 10:55:05 +0530

    Updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,10 @@ export default function Home() {
     <div className="home-page space-y-8 md:space-y-16">
       <section className="hero-section text-center px-4 md:px-6">
         <h1 className="text-3xl md:text-4xl font-bold py-6 md:py-12 leading-tight">
-        Growth Strategies that Scale
+        SaaS Growth Strategies that Scale
         </h1>
         <h2 className="text-xl md:text-2xl font-semibold py-8 md:py-16 px-4 md:px-20 leading-normal">
-        Growth tactics that scale & build value long-term
+        We execute across SEO, Social Media, Ads, and Referral platforms to help you grow your business.
         </h2>
       </section>
 
@@ -27,7 +27,10 @@ export default function Home() {
           </div>
           <div className="md:w-1/2 md:pl-12">
             <p className="text-gray-700 mb-4">
-              At RocketLift, our mission is to empower businesses with cutting-edge growth strategies that drive success and scalability. Our dedicated team of experts is committed to delivering exceptional results tailored to your unique needs.
+              We believe Growth is about doing a bunch of experiments, learning what works and what doesn't, and then scaling them.
+            </p>
+            <p className="text-gray-700 mb-4">
+              We bring a toolkit that includes SEO, Content Marketing, Ads, and Referral platforms to help you grow your business.
             </p>
             <p className="text-gray-700">
               Join us on our journey to revolutionize the industry and achieve remarkable growth together.
